e9e8c5cf3d64dd3bd8f44ecfc7a0381112258df4
JonYang1 <1109340909@qq.com>
2020-05-16 11:14:43 +0800

    构建测试界面

---
+++ app/src/main/java/com/example/domemy/ProduceActive.kt
@@ -0,0 +1,13 @@
+package com.example.domemy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProduceActive: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //加载top 视图
+        setContentView(R.layout.produce_active)
+    }
+
+}
